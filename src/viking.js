71e79d2caad62337090aed85a2ccfa0de973cb6c
Loreley <Laura.Thiel.Me@gmail.com>
2021-01-18 18:20:34 +0100

    first trial

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,30 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength
+    }
+    attack() {
+        return this.strength
+    }
+    receiveDamage(damage) {
+        this.health = this.health - damage
+        this.damage = this.damage - damage
+
+   }
+} 
+
 
 // Viking
-class Viking {}
+//class Viking {
+//    constructor (name, health, strength) {
+//        super (health, strength)
+//        this.name = name
+//        this.health = health
+//        this.strength = strength;
+//    }
+
+//}
 
 // Saxon
 class Saxon {}
